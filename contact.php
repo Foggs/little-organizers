37bee59d29fcf593cee9ce00698e0f1fbda82618
rfoggo <admin@robfoggo.com>
2018-04-25 10:00:39 -0400

    contact form added for Testing

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Demo contact form <info@littleorganizers.com>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <robfoggo@gmail.com>';
+$sendTo = 'Demo contact form <annmarielittle@yahoo.com>';
 
 // subject of the email
 $subject = 'New message from contact form';
--- js/main.js
+++ js/main.js
@@ -1,24 +1,13 @@
 // Scrolls to the selected menu item on the page
-// $(function() {
-
-//     $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//             if (target.length) {
-//                 $('html,body').animate({
-//                     scrollTop: target.offset().top - 40
-//                 }, 1000);
-//                 return false;
-//             }
-//         }
-//     });
-// });
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a:not(".dropdown-toggle")') ) {
         $(this).collapse('hide');
     }
 });
+
+
+
+
 
 //#to-top button appears after scrolling
 var fixed = false;
